24f436089b37c5d5ea002ed590e1efb4687c076c
prince raj <prince.raj@salescode.ai>
2025-01-16 18:08:59 +0530

    js

---
+++ src/product.js
@@ -0,0 +1,49 @@
+const portfolio = [
+  {
+    "name": "vic-ecommerce",
+    "details": {
+      banners: [
+        "vic-ecom/vic-ecommerce.png",
+        "vic-ecom/vic-ecom1.png",
+        "vic-ecom/vic-ecom2.png",
+        "vic-ecom/vic-ecom3.png"
+      ],
+      "punchline": "Your Ultimate Online Shopping Destination",
+      "title": "NextGen E-Commerce Platform",
+      "desc": "The NextGen E-Commerce Platform is a comprehensive and user-friendly online shopping solution, designed to rival the best in the industry. Packed with advanced features and intuitive designs, this platform delivers a seamless shopping experience for customers while offering robust tools for merchants. With features like dynamic product listings, secure payment gateways, personalized recommendations, advanced search, and real-time tracking, it’s the perfect combination of functionality and innovation. Whether you're building a marketplace for retail, electronics, fashion, or groceries, this platform provides everything you need to scale and succeed in the e-commerce world.",
+      "type": "filter-app"
+    }
+  }
+];
+const renderPortfoliPage = () => {
+  portfolio.forEach(item => {
+    const htmlContent = `
+        <div class="col-lg-4 col-md-6 portfolio-item isotope-item ${item.details.type}">
+          <img src="assets/img/portfolio/${item.details.banners[0]}" class="img-fluid" alt="">
+          <div class="portfolio-info">
+            <h4>${item.details.title}</h4>
+            <p>${item.details.punchline}</p>
+            <a href="${item.details.banners[0]}" title="${item.details.title}"
+               data-gallery="portfolio-gallery-app" class="glightbox preview-link">
+              <i class="bi bi-zoom-in"></i>
+            </a>
+            <a href="portfolio-details.html?${item.name}" title="More Details" class="details-link">
+              <i class="bi bi-link-45deg"></i>
+            </a>
+          </div>
+        </div>
+      `;
+    renderProducts.innerHTML += htmlContent;
+  });
+}
+const renderProducts = document.getElementById('renderProducts');
+
+if (window.location.pathname == '/portfolio.html') {
+  renderPortfoliPage()
+} else {
+  const fullUrl = window.location.href;
+  const queryString = fullUrl.split('?')[1];
+  const extractedValue = queryString ? queryString.split('=')[1] : null;
+  console.log("Full URL:", fullUrl);
+  console.log("Extracted Value:", extractedValue);
+}

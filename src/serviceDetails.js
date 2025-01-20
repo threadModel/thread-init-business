43b59acd1cacdcbff5412aeab3111468bf357c36
prince raj <prince.raj@salescode.ai>
2025-01-20 18:33:00 +0530

    footer done

---
+++ src/serviceDetails.js
@@ -0,0 +1,226 @@
+const ServiceData = [
+    {
+        name: "webDevelopment",
+        title: "Web Development",
+        banner: "assets/img/services.jpg",
+        pdf: "#",
+        doc: "/",
+        content: `
+          <img src="{BANNER}" alt="Service Banner" class="img-fluid services-img">
+          <h3>Temporibus et in vero dicta aut eius lidero plastis trand lined voluptas dolorem ut voluptas</h3>
+          <p>
+            Blanditiis voluptate odit ex error ea sed officiis deserunt. Cupiditate non consequatur et doloremque
+            consequuntur. Accusantium labore reprehenderit error temporibus saepe perferendis fuga doloribus vero. Qui
+            omnis quo sit. Dolorem architecto eum et quos deleniti officia qui.
+          </p>
+          <ul>
+            <li><i class="bi bi-check-circle"></i> <span>Aut eum totam accusantium voluptatem.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Assumenda et porro nisi nihil nesciunt voluptatibus.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea</span></li>
+          </ul>
+          <p>
+            Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste
+            veniam repellat odit. Aut pariatur itaque nesciunt fuga.
+          </p>
+          <p>
+            Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia
+            doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet
+            velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum.
+            Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto
+            ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.
+          </p>
+        `,
+    },
+    {
+        name: "maintaines&SoftwareSolution",
+        title: "Maintaines & Software Solution",
+        banner: "assets/img/services.jpg",
+        pdf: "#",
+        doc: "/",
+        content: `
+          <img src="{BANNER}" alt="Service Banner" class="img-fluid services-img">
+          <h3>Temporibus et in vero dicta aut eius lidero plastis trand lined voluptas dolorem ut voluptas</h3>
+          <p>
+            Blanditiis voluptate odit ex error ea sed officiis deserunt. Cupiditate non consequatur et doloremque
+            consequuntur. Accusantium labore reprehenderit error temporibus saepe perferendis fuga doloribus vero. Qui
+            omnis quo sit. Dolorem architecto eum et quos deleniti officia qui.
+          </p>
+          <ul>
+            <li><i class="bi bi-check-circle"></i> <span>Aut eum totam accusantium voluptatem.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Assumenda et porro nisi nihil nesciunt voluptatibus.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea</span></li>
+          </ul>
+          <p>
+            Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste
+            veniam repellat odit. Aut pariatur itaque nesciunt fuga.
+          </p>
+          <p>
+            Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia
+            doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet
+            velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum.
+            Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto
+            ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.
+          </p>
+        `,
+    },
+    {
+        name: "applicationDevelopment",
+        title: "Application Development",
+        banner: "assets/img/services.jpg",
+        pdf: "#",
+        doc: "/",
+        content: `
+          <img src="{BANNER}" alt="Service Banner" class="img-fluid services-img">
+          <h3>Temporibus et in vero dicta aut eius lidero plastis trand lined voluptas dolorem ut voluptas</h3>
+          <p>
+            Blanditiis voluptate odit ex error ea sed officiis deserunt. Cupiditate non consequatur et doloremque
+            consequuntur. Accusantium labore reprehenderit error temporibus saepe perferendis fuga doloribus vero. Qui
+            omnis quo sit. Dolorem architecto eum et quos deleniti officia qui.
+          </p>
+          <ul>
+            <li><i class="bi bi-check-circle"></i> <span>Aut eum totam accusantium voluptatem.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Assumenda et porro nisi nihil nesciunt voluptatibus.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea</span></li>
+          </ul>
+          <p>
+            Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste
+            veniam repellat odit. Aut pariatur itaque nesciunt fuga.
+          </p>
+          <p>
+            Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia
+            doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet
+            velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum.
+            Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto
+            ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.
+          </p>
+        `,
+    },
+    {
+        name: "aIPluginIntegration",
+        title: "AI Plugin Integration",
+        banner: "assets/img/services.jpg",
+        pdf: "#",
+        doc: "/",
+        content: `
+          <img src="{BANNER}" alt="Service Banner" class="img-fluid services-img">
+          <h3>Temporibus et in vero dicta aut eius lidero plastis trand lined voluptas dolorem ut voluptas</h3>
+          <p>
+            Blanditiis voluptate odit ex error ea sed officiis deserunt. Cupiditate non consequatur et doloremque
+            consequuntur. Accusantium labore reprehenderit error temporibus saepe perferendis fuga doloribus vero. Qui
+            omnis quo sit. Dolorem architecto eum et quos deleniti officia qui.
+          </p>
+          <ul>
+            <li><i class="bi bi-check-circle"></i> <span>Aut eum totam accusantium voluptatem.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Assumenda et porro nisi nihil nesciunt voluptatibus.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea</span></li>
+          </ul>
+          <p>
+            Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste
+            veniam repellat odit. Aut pariatur itaque nesciunt fuga.
+          </p>
+          <p>
+            Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia
+            doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet
+            velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum.
+            Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto
+            ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.
+          </p>
+        `,
+    },
+    {
+        name: "hosting&Deployment",
+        title: "Hosting & Deployment",
+        banner: "assets/img/services.jpg",
+        pdf: "#",
+        doc: "/",
+        content: `
+          <img src="{BANNER}" alt="Service Banner" class="img-fluid services-img">
+          <h3>Temporibus et in vero dicta aut eius lidero plastis trand lined voluptas dolorem ut voluptas</h3>
+          <p>
+            Blanditiis voluptate odit ex error ea sed officiis deserunt. Cupiditate non consequatur et doloremque
+            consequuntur. Accusantium labore reprehenderit error temporibus saepe perferendis fuga doloribus vero. Qui
+            omnis quo sit. Dolorem architecto eum et quos deleniti officia qui.
+          </p>
+          <ul>
+            <li><i class="bi bi-check-circle"></i> <span>Aut eum totam accusantium voluptatem.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Assumenda et porro nisi nihil nesciunt voluptatibus.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea</span></li>
+          </ul>
+          <p>
+            Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste
+            veniam repellat odit. Aut pariatur itaque nesciunt fuga.
+          </p>
+          <p>
+            Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia
+            doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet
+            velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum.
+            Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto
+            ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.
+          </p>
+        `,
+    },
+    {
+        name: "teamAugmentation",
+        title: "Team Augmentation",
+        banner: "assets/img/services.jpg",
+        pdf: "#",
+        doc: "/",
+        content: `
+          <img src="{BANNER}" alt="Service Banner" class="img-fluid services-img">
+          <h3>Temporibus et in vero dicta aut eius lidero plastis trand lined voluptas dolorem ut voluptas</h3>
+          <p>
+            Blanditiis voluptate odit ex error ea sed officiis deserunt. Cupiditate non consequatur et doloremque
+            consequuntur. Accusantium labore reprehenderit error temporibus saepe perferendis fuga doloribus vero. Qui
+            omnis quo sit. Dolorem architecto eum et quos deleniti officia qui.
+          </p>
+          <ul>
+            <li><i class="bi bi-check-circle"></i> <span>Aut eum totam accusantium voluptatem.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Assumenda et porro nisi nihil nesciunt voluptatibus.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea</span></li>
+          </ul>
+          <p>
+            Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste
+            veniam repellat odit. Aut pariatur itaque nesciunt fuga.
+          </p>
+          <p>
+            Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia
+            doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet
+            velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum.
+            Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto
+            ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.
+          </p>
+        `,
+    },
+];
+
+const renderServiceDetail = (queryPrompt) => {
+    const service = ServiceData.find((p) => p.name === queryPrompt);
+    const serviceContent = document.getElementById("serviceContent");
+    const servicesList = document.getElementById("services-list");
+    const pdf = document.getElementById("pdf");
+    const doc = document.getElementById("doc");
+
+    if (!service) {
+        serviceContent.innerHTML = "<p>Service not found.</p>";
+        return;
+    }
+    pdf.setAttribute("href", service.pdf);
+    doc.setAttribute("href", service.doc);
+    const links = servicesList.querySelectorAll("a");
+    links.forEach((link) => {
+        const hrefText = link.getAttribute("href").split("?")[1]; // Get the text after '?'
+        if (hrefText === queryPrompt) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+
+    const renderedContent = service.content.replace("{BANNER}", service.banner);
+    serviceContent.innerHTML = renderedContent;
+
+};
+
+const fullUrl = window.location.href;
+const queryString = fullUrl.split('?')[1];
+renderServiceDetail(queryString);
